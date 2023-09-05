6bdda7c39ff7309e9f187037f0bf785d50c986bf
Marcin Hodor <mhodor@o2.pl>
2023-09-05 21:44:24 +0200

    first commit

---
+++ src/components/ArticleCard.svelte
@@ -0,0 +1,193 @@
+<script>
+  import { Circle } from "svelte-loading-spinners"
+  import { fade } from "svelte/transition"
+  export let article
+
+  let summary = ""
+  let loading = false
+
+  const authors_formatted = (authors) => {
+    let text = ""
+    for (const author of authors) {
+      if (author === authors.at(-1)) {
+        text = text + author
+      } else {
+        text = text + author + ", "
+      }
+    }
+    return text
+  }
+
+  const formatDotSpace = (text) => {
+    return text.replace(/ \./g, ".")
+  }
+
+  const downloadSummary = async (text) => {
+    loading = true
+    const url = `https://articlesummarize-1-z7868205.deta.app/summarize`
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    })
+    const data = await response.json()
+    summary = formatDotSpace(data[0].summary_text)
+    loading = false
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="card">
+  <div class="left">
+    <img src={article.image} alt="article" />
+  </div>
+  <div class="middle">
+    <h1 class="title">{article.title}</h1>
+    <p class="authors">{authors_formatted(article.authors)}</p>
+    {#if loading}
+      <div class="spinner" in:fade>
+        <Circle size="40" color="var(--almost-black)" unit="px" duration="1s" />
+      </div>
+    {:else}
+      <p class="summary">
+        {summary}
+      </p>
+    {/if}
+  </div>
+  <div class="right">
+    <button class="button-top" on:click={downloadSummary(article.text)}
+      >Summarize</button
+    >
+    <a
+      href={article.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      on:click|stopPropagation={""}
+      ><button class="button-bottom">Read full</button></a
+    >
+  </div>
+</div>
+
+<style lang="scss">
+  .card {
+    display: grid;
+    grid-template-columns: 160px minmax(0, 800px) 120px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    margin: 8px;
+    border-radius: 5px;
+    box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.2);
+    padding: 8px;
+
+    // &:hover {
+    //   background-color: #fafafa;
+    // }
+  }
+
+  .left {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 8px;
+
+    img {
+      max-width: 150px;
+      // max-height: 100px;
+      border-radius: 4px;
+    }
+  }
+
+  .middle {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+
+    .title {
+      font-size: 1.2rem;
+      color: var(--almost-black);
+    }
+
+    .authors {
+      margin: 5px 0;
+      color: #666666;
+      font-size: 0.9rem;
+    }
+    .spinner {
+      display: flex;
+      justify-content: center;
+    }
+
+    .summary {
+      // font-style: italic;
+      color: var(--almost-black);
+    }
+  }
+
+  .right {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    justify-content: center;
+    margin-left: 8px;
+
+    .button-top {
+      margin-bottom: 8px;
+      color: var(--dark-blue);
+      background-color: var(--white);
+      border: 1px solid var(--dark-blue);
+
+      &:hover {
+        background-color: var(--light-grey);
+      }
+    }
+
+    .button-bottom {
+      background-color: var(--dark-blue);
+    }
+
+    button {
+      border: none;
+      color: #fff;
+      padding: 10px 12px;
+      text-align: center;
+      text-decoration: none;
+      font-size: 14px;
+      width: 100px;
+      border-radius: 4px;
+      z-index: 1;
+
+      &:hover {
+        background-color: var(--dark-blue-hover);
+        cursor: pointer;
+      }
+    }
+  }
+
+  @media (max-width: 600px) {
+    .card {
+      grid-template-columns: auto auto;
+      grid-template-rows: auto auto;
+
+      .left {
+        order: 1;
+        margin-bottom: 8px;
+      }
+
+      .middle {
+        order: 3;
+        grid-column: span 2;
+      }
+
+      .right {
+        order: 2;
+        align-items: center;
+      }
+    }
+  }
+</style>
---
+++ src/components/Articles.svelte
@@ -0,0 +1,82 @@
+<script>
+  import { onMount } from "svelte"
+  import { fade } from "svelte/transition"
+  import websites from "../data/websites.json"
+
+  // import data_file from "../data/dev_data.json"
+  import ArticleCard from "./ArticleCard.svelte"
+  import { Circle } from "svelte-loading-spinners"
+
+  export let endpoint
+
+  let articles = []
+  let loading = false
+
+  const imgAndTitle = (endpoint) => {
+    const index = websites.findIndex(
+      (website) => "/" + website.short_name === endpoint
+    )
+    // console.log(websites[0].short_name)
+    const websiteInfo = websites[index]
+    // console.log(websiteInfo)
+    return websiteInfo
+  }
+
+  onMount(async function () {
+    loading = true
+    const url = `https://articlesummarize-1-z7868205.deta.app/articles${endpoint}`
+    const response = await fetch(url)
+    const data = await response.json()
+    articles = data
+    // articles = data_file
+    loading = false
+  })
+</script>
+
+<div class="articles">
+  <div class="website-img">
+    <img src={imgAndTitle(endpoint).image_url} alt="news website" />
+  </div>
+  {#if loading}
+    <div class="spinner" in:fade>
+      <Circle size="80" color="var(--dark-blue)" unit="px" duration="1s" />
+      <p>Please give me a moment. I'm fetching the articles...</p>
+    </div>
+  {:else}
+    <div in:fade>
+      {#each articles as article}
+        <ArticleCard {article} />
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .website-img {
+    display: flex;
+    margin-top: 8px;
+    justify-content: center;
+    img {
+      max-width: max(150px, 20%);
+      border-radius: 5px;
+    }
+  }
+
+  .articles {
+    align-self: center;
+    width: min(1114px, 100%);
+    // border: 1px solid red;
+
+    .spinner {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: min(200px, calc(0.2 * 100vw));
+
+      p {
+        margin-top: 2rem;
+        color: var(--almost-black);
+      }
+    }
+  }
+</style>
